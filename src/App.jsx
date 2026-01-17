58c4f7b062e8a89106f02ddae2f3fb0fb9ed251b
afiab <afiabidica@gmail.com>
2026-01-17 02:49:18 -0500

    adding content to the page but in react, mostly in the previous commit actually

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import './App.css'
+import MainContent from './MainContent'
 import Sidebar from './Sidebar'
 
 function App() {
-  
+
+  const projectData = [
+  {
+    type: "text",
+    header: "Software Engineering",
+    content: "blah blah lorem ipsum dolor sit amet place holder text..."
+  },
+  {
+    type: "image-overlay",
+    imageUrl: "/public/placeholder.png",
+    title: "Title here",
+    subtitle: "Month Year to Month Year",
+    link: "/swe" // The destination URL
+  },
+  {
+    type: "text",
+    header: "Graphic Design",
+    content: "blah blah lorem ipsum dolor sit amet place holder text..."
+  }
+];
+
   return (
     <>
       <Sidebar />
+      <MainContent sections={projectData} />
     </>
   )
 }
