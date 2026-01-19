b510707c587c30ddb2f8bd94602190ba87bac3f3
afiab <afiabidica@gmail.com>
2026-01-18 20:23:37 -0500

    removing image from contact page

--- src/ContactApp.jsx
+++ src/ContactApp.jsx
@@ -20,13 +20,6 @@ function ContactApp() {
         {
             type: "paragraph",
             content: "Additional paragraph content specific to Contact + Resume..."
-        },
-        {
-            type: "image-overlay",
-            imageUrl: "/public/placeholder.gif",
-            title: "Projects & Experience",
-            subtitle: "Month Year to Month Year",
-            link: "/business" // The destination URL
         }
     ];
 
