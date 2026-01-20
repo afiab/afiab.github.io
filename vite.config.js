69f8d6eb7ce0af38d9fb950e7d96540ca3e33f8b
afiab <afiabidica@gmail.com>
2026-01-20 05:27:11 -0500

    apparently it was just supposed to be a / for the base name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // Ensure your base path is correct for GH Pages
-  base: '/afiab.github.io/', 
+  base: '/', 
   plugins: [react()],
   build: {
     rollupOptions: {
